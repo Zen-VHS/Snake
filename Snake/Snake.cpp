b6a39645177deed7d67c2b18ed88455989e145c1
ZEN <nekomata.j@gmail.com>
2021-05-26 14:01:14 +0300

    Добален рандомный спавн фрукта и соответствующие библиотеки, добалена библиотека отслеживающая нажатия клавиатуры

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,47 +1,75 @@
 #include <iostream>
+#include <conio.h> // автономно отслеживает нажатия
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
 bool gameOver;
 const int width = 20;
-const int heigh = 20;
+const int height = 20;
 int x, y, fruitX, fruitY, score;
-enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
 eDirection dir;
 
 
 void Setup() {
     gameOver = false;
+    srand(time(NULL) + 1);
     dir = STOP;
-    x = width / 2;
-    y = heigh / 2;
+    x = width / 2 - 1;
+    y = height / 2 - 1;
     fruitX = rand() % width;
-    fruitX = rand() % heigh;
+    fruitX = rand() % height;
 
 }
 
 void Draw() {
     system("cls"); // system ("clear");
-    for (int i = 0; i < width + 1; i++)
+    for (int i = 0; i < width + 1; i++) // рисует верхнюю границу
         cout << "#";
     cout << endl;
 
-    for (int i = 0; i < heigh; i++) {
+    for (int i = 0; i < height; i++) { // рисует границы по бокам
         for (int j = 0; j < width; j++) {
             if (j == 0 || j == width - 1)
                 cout << "#";
-            cout << " ";
+            if (i == y && j == x)
+                cout << "0";
+            else if (i == fruitY && j == fruitX)
+                cout << "F";
+            else
+                cout << " ";
         }
         cout << endl;
     }
 
-    for (int i = 0; i < width + 1; i++)
+    for (int i = 0; i < width + 1; i++) // рисует нижнюю границу
         cout << "#";
     cout << endl;
 }
 
 void Input() {
-
+    if (_kbhit()) {
+        switch (_getch())
+        {
+        case 'a':
+            dir = LEFT;
+            break;
+        case 'd':
+            dir = RIGHT;
+            break;
+        case 'w':
+            dir = UP;
+            break;
+        case 's':
+            dir = DOWN;
+            break;
+        case 'x':
+            gameOver = true;
+            break;
+        }
+    }
 }
 
 void Logic() {
