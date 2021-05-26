2a1cea5486bcfa37031127c41fdd64365e7662b0
ZEN <nekomata.j@gmail.com>
2021-05-26 13:06:33 +0300

    Создал заготовки функций, добавил булеву переменную gameOver

--- Snake/Snake.cpp
+++ Snake/Snake.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
+using namespace std;
+
+bool gameOver;
+
+void Setup() {
+
+}
+
+void Draw() {
+
+}
+
+void Input() {
+
+}
+
+void Logic() {
+
+}
+
 int main()
 {
+
 
     return 0;
 }
